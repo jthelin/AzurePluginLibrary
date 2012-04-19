f28291aa4ebce3b33262e5e24c86a328e310540d
Richard Astbury <Richard.Astbury@gmail.com>
2012-04-19 20:02:22 +0100

    added utils

---
+++ utils/DownloadFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace DownloadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("please specify the URL and the local filename as the first two arguments");
+                return;
+            }
+            try
+            {
+                var client = new WebClient();
+                client.DownloadFile(args[0], args[1]);
+                Console.WriteLine("Success");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Environment.ExitCode = -1;
+            }
+        }
+    }
+}
---
+++ utils/ReadSetting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace ReadSetting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("You must specify the name of the setting to retrieve");
+                return;
+            }
+            Console.WriteLine(RoleEnvironment.GetConfigurationSettingValue(args[0]));
+        }
+    }
+}
