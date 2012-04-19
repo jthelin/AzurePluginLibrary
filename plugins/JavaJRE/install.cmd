b121c6687395e33ba245012a1d0d557fdef11116
Rob Blackwell <rob.blackwell@aws.net>
2012-04-19 16:15:03 +0100

    Fixed Java JRE - installer now in Blob store to avoid GIT file size limit

--- plugins/JavaJRE/install.cmd
+++ plugins/JavaJRE/install.cmd
@@ -1,4 +1,2 @@
-jre-6u31-windows-x64.exe /s 
-date /t > log.txt
-time /t >> log.txt
-exit /b 0
+..\Curl\curl.exe -k -o jre-6u31-windows-x64.exe https://robblackwell.blob.core.windows.net/public/jre-6u31-windows-x64.exe
+jre-6u31-windows-x64.exe /s
