cc8ba45c3f30d6b95973500efce56ac6704688ea
Richard Astbury <Richard.Astbury@gmail.com>
2012-04-19 12:37:02 +0100

    added DisableIESecurity

---
+++ plugins/DisableIESecurity/setup.cmd
@@ -0,0 +1,3 @@
+
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" /v "IsInstalled" /t REG_DWORD /d 0 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" /v "IsInstalled" /t REG_DWORD /d 0 /f
