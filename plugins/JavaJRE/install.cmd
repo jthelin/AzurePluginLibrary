fc04753bb80459958d14d4666818f51da2dfbc7d
Richard Astbury <Richard.Astbury@gmail.com>
2012-05-09 14:03:18 +0100

    removed dependencies between projects

--- plugins/JavaJRE/install.cmd
+++ plugins/JavaJRE/install.cmd
@@ -1,2 +1,2 @@
-..\Curl\curl.exe -k -o jre-6u31-windows-x64.exe https://robblackwell.blob.core.windows.net/public/jre-6u31-windows-x64.exe
+DownloadFile.exe https://robblackwell.blob.core.windows.net/public/jre-6u31-windows-x64.exe jre-6u31-windows-x64.exe 
 jre-6u31-windows-x64.exe /s
--- plugins/Tomcat/install.cmd
+++ plugins/Tomcat/install.cmd
@@ -1,5 +1,5 @@
-..\Curl\curl.exe -o apache-tomcat-7.0.27-windows-x64.zip http://mirror.lividpenguin.com/pub/apache/tomcat/tomcat-7/v7.0.27/bin/apache-tomcat-7.0.27-windows-x64.zip
-..\SevenZip\7z.exe x apache-tomcat-7.0.27-windows-x64.zip
+DownloadFile.exe http://mirror.lividpenguin.com/pub/apache/tomcat/tomcat-7/v7.0.27/bin/apache-tomcat-7.0.27-windows-x64.zip apache-tomcat-7.0.27-windows-x64.zip
+7z.exe x apache-tomcat-7.0.27-windows-x64.zip
 set CATALINA_HOME=%cd%\apache-tomcat-7.0.27
 cd /d %windir%"\..\Program Files\Java\jre6"
 SET JRE_HOME=%CD%
