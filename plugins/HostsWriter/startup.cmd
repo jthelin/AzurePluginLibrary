15fd339dd3349e27e6372f5cf24e0c8641da9050
Richard Astbury <Richard.Astbury@gmail.com>
2012-10-10 15:31:33 +0100

    Added a host file writing plugin

---
+++ plugins/HostsWriter/startup.cmd
@@ -0,0 +1,2 @@
+HostfileWriter.exe > %WINDIR%\system32\drivers\etc\hosts
+PING 1.1.1.1 -n 1 -w 60000 >NUL
---
+++ utils/HostfileWriter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace HostfileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var role in RoleEnvironment.Roles.Values)
+            {
+                foreach (var instance in role.Instances.Where(x => x.InstanceEndpoints.Count > 0))
+                {
+                    var endpoint = instance.InstanceEndpoints.FirstOrDefault();
+                    Console.WriteLine(string.Format("{0} {1}", instance.InstanceEndpoints.Values.First().IPEndpoint.Address, instance.Id));
+
+                }
+            }
+        }
+    }
+}
