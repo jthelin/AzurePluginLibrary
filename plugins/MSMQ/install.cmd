4677932f34c074a45c3699942eb5f844a4b76bbf
Richard Astbury <Richard.Astbury@gmail.com>
2012-10-31 15:32:27 +0000

    drop port 135 on the firewall, as it's not connecting to the windows service

--- plugins/MSMQ/install.cmd
+++ plugins/MSMQ/install.cmd
@@ -1,6 +1,6 @@
 powershell -command "Set-ExecutionPolicy Unrestricted"
 powershell .\InstallMSMQ.ps1
 
-cscript setupsmtpserver.vbs "%Two10.SMTP.Alias%"
+netsh advfirewall firewall add rule name="Open port to Service" dir=in localport=135 protocol=TCP action=allow
 
 exit /b 0
