e8cc87f4489dd8310f61b76298831e1498059231
Richard Astbury <Richard.Astbury@gmail.com>
2013-03-06 10:24:45 +0000

    updated tomcat version

--- plugins/Tomcat/install.cmd
+++ plugins/Tomcat/install.cmd
@@ -1,6 +1,6 @@
-DownloadFile.exe http://mirror.lividpenguin.com/pub/apache/tomcat/tomcat-7/v7.0.27/bin/apache-tomcat-7.0.27-windows-x64.zip apache-tomcat-7.0.27-windows-x64.zip
-7z.exe x apache-tomcat-7.0.27-windows-x64.zip
-set CATALINA_HOME=%cd%\apache-tomcat-7.0.27
+DownloadFile.exe http://mirror.lividpenguin.com/pub/apache/tomcat/tomcat-7/v7.0.37/bin/apache-tomcat-7.0.37-windows-x64.zip apache-tomcat-7.0.37-windows-x64.zip
+7z.exe x apache-tomcat-7.0.37-windows-x64.zip
+set CATALINA_HOME=%cd%\apache-tomcat-7.0.37
 cd /d %windir%"\..\Program Files\Java\jre6"
 SET JRE_HOME=%CD%
 cd /d %CATALINA_HOME%\bin
