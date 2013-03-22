eae57fbd90fef5b7ae5b5e3bb91f2045cd708537
Richard Astbury <Richard.Astbury@gmail.com>
2013-03-22 14:05:46 +0000

    updating node plugin to 10.0.1

--- plugins/Node/install.cmd
+++ plugins/Node/install.cmd
@@ -1,3 +1,3 @@
-msiexec /I node-v0.6.15.msi /quiet
+msiexec /I node-v0.10.1-x64.msi /quiet
 AppendPath.exe "%programfiles(x86)%\nodejs"
 netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles(x86)%\nodejs\node.exe" enable=yes
