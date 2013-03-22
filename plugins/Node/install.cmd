7f260c4d97a7113af441db12aac6fd5336b27dab
Richard Astbury <Richard.Astbury@gmail.com>
2013-03-22 14:58:08 +0000

    node 10 has moved!

--- plugins/Node/install.cmd
+++ plugins/Node/install.cmd
@@ -1,3 +1,3 @@
 msiexec /I node-v0.10.1-x64.msi /quiet
-AppendPath.exe "%programfiles(x86)%\nodejs"
-netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles(x86)%\nodejs\node.exe" enable=yes
+AppendPath.exe "%programfiles%\nodejs"
+netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles%\nodejs\node.exe" enable=yes
