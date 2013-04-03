2d52fcb68a34a82b49853336b9222e6b5ddf2934
Richard Astbury <Richard.Astbury@gmail.com>
2013-04-03 13:40:19 +0100

    added logging

--- plugins/Node/install.cmd
+++ plugins/Node/install.cmd
@@ -1,3 +1,3 @@
-msiexec /I node-v0.8.22-x64.msi /quiet
-AppendPath.exe "%programfiles%\nodejs"
-netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles%\nodejs\node.exe" enable=yes
+msiexec /I node-v0.8.22-x64.msi /quiet >> install.log
+AppendPath.exe "%programfiles%\nodejs" >> install.log
+netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles%\nodejs\node.exe" enable=yes >> install.log
--- plugins/Node/start.cmd
+++ plugins/Node/start.cmd
@@ -1,9 +1,9 @@
 cd \AppRoot
-"%programfiles(x86)%\nodejs\npm.cmd" install
-"%programfiles(x86)%\nodejs\node.exe" server.js
-"%programfiles(x86)%\nodejs\node.exe" app.js
+"%programfiles(x86)%\nodejs\npm.cmd" install >> start.log
+"%programfiles(x86)%\nodejs\node.exe" server.js >> start.log
+"%programfiles(x86)%\nodejs\node.exe" app.js >> start.log
 cd bin
-"%programfiles(x86)%\nodejs\npm.cmd" install
-"%programfiles(x86)%\nodejs\node.exe" server.js
-"%programfiles(x86)%\nodejs\node.exe" app.js
+"%programfiles(x86)%\nodejs\npm.cmd" install >> start.log
+"%programfiles(x86)%\nodejs\node.exe" server.js >> start.log
+"%programfiles(x86)%\nodejs\node.exe" app.js >> start.log
 
