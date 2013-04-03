012bf1a9cbc3fba3ac9e71db51fcfbb6a0b7da1a
Richard Astbury <Richard.Astbury@gmail.com>
2013-04-03 13:21:22 +0100

    downgrade to node 8.22

--- plugins/Node/install.cmd
+++ plugins/Node/install.cmd
@@ -1,3 +1,3 @@
-msiexec /I node-v0.10.1-x64.msi /quiet
+msiexec /I node-v0.8.22-x64.msi /quiet
 AppendPath.exe "%programfiles%\nodejs"
 netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles%\nodejs\node.exe" enable=yes
