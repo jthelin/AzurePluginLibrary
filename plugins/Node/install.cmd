1d2b1795b65ff5a1929fb3aaf4197fae081a07c4
Richard Astbury <Richard.Astbury@gmail.com>
2013-05-16 21:12:31 +0100

    added logging to the node install

--- plugins/Node/install.cmd
+++ plugins/Node/install.cmd
@@ -1,3 +1,3 @@
-msiexec /I node-v0.8.22-x64.msi /quiet >> install.log
+msiexec /I node-v0.8.22-x64.msi /quiet /l nodeinstall.log >> install.log
 AppendPath.exe "%programfiles%\nodejs" >> install.log
 netsh advfirewall firewall add rule name="Node.js" dir=in action=allow program="%programfiles%\nodejs\node.exe" enable=yes >> install.log
