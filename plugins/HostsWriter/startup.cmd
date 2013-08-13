133aab3b1cef44c0361a86bfe4553d57836b415b
Richard Astbury <richard.astbury@gmail.com>
2013-08-13 11:48:48 +0100

    updated Hosts Writer

--- plugins/HostsWriter/startup.cmd
+++ plugins/HostsWriter/startup.cmd
@@ -1,6 +1,8 @@
 :START
 
-HostfileWriter.exe > %WINDIR%\system32\drivers\etc\hosts
+HostfileWriter.exe 
+
+copy hosts %WINDIR%\system32\drivers\etc\hosts /Y
 
 PING 1.1.1.1 -n 1 -w 60000 >NUL
 
--- utils/HostfileWriter/Program.cs
+++ utils/HostfileWriter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using System.IO;
 
 namespace HostfileWriter
 {
@@ -10,12 +11,13 @@ namespace HostfileWriter
     {
         static void Main(string[] args)
         {
+            using (var sw = new StreamWriter(@"hosts"))
             foreach (var role in RoleEnvironment.Roles.Values)
             {
                 foreach (var instance in role.Instances.Where(x => x.InstanceEndpoints.Count > 0))
                 {
+                    sw.WriteLine(string.Format("{0} {1}", instance.InstanceEndpoints.Values.First().IPEndpoint.Address, instance.Id));
                     Console.WriteLine(string.Format("{0} {1}", instance.InstanceEndpoints.Values.First().IPEndpoint.Address, instance.Id));
-
                 }
             }
         }
